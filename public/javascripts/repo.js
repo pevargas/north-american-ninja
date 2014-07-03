4fa1bcdef90de799add5e1afe64e6106b6f3ece2
Patrick <pevargas90@gmail.com>
2014-07-02 21:03:54 -0600

    Added assets

---
+++ public/javascripts/repo.js
@@ -0,0 +1,206 @@
+// Patrick E. Vargas 
+// www.vargascorpus.com
+
+var repo = {
+  /////////////////////////////////////////////////////////////////////////////
+  // Business Cards
+  "business-cards-anonymous.png" :
+  { 
+    date: "2013-04",
+    text: "Designed business cards in Inkscape. This is one of the alternate backs. This quote is in the style of HTML and reads, \"Everything will be okay in the end. If it's not okay, it's not the end\" by Anonymous."
+  },
+  "business-cards-lucille-ball.png" :
+  { 
+    date: "2013-04",
+    text: "Designed business cards in Inkscape. This is one of the alternate backs. This quote is in the style of Latex and reads, \"I'd rather regret the things I've done than regret the things I haven't done.\" by Lucille Ball."
+  },
+  "business-cards-main.png" :
+  { 
+    date: "2013-04",
+    text: "Designed business cards in Inkscape. This is the main side of the business card."
+  },
+  "business-cards-oscar-wilde.png" :
+  { 
+    date: "2013-04",
+    text: "Designed business cards in Inkscape. This is one of the alternate backs. This quote is in the style of the R programming language and reads, \"Be yourself; Everyone else is already taken.\" By Oscar Wilde."
+  },
+  "business-cards-zig-ziglar.png" :
+  { 
+    date: "2013-04",
+    text: "Designed business cards in Inkscape. This is one of the alternate backs. This quote is in the style of C++ and reads, \"The chief cause of failure and unhappiness is trading what you want most for what you want right now.\" by Zig Ziglar"
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // GSA Flyer
+  "flyer-gsa-drag-show-jungle.png":
+  {
+    date: "2010-08",
+    text: "Designed flyers for the Gay-Straight Alliance. This flyer was used for the annual Drag Show, which had a jungle theme"
+  },
+  "flyer-gsa-queer-sex-ed-02.png":
+  {
+    date: "2010-09",
+    text: "Designed flyers for the Gay-Straight Alliance. This alternate flyer was for the annual Queer Sex Ed event."
+  },
+  
+  /////////////////////////////////////////////////////////////////////////////
+  // SAGE Flyer
+  "flyer-sage-general-meeting.png":
+  {
+    date: "2011-11",
+    text: "Designed flyers for the Student Alliance of GLBT Engineers. This promotional handbill was used to gain new members"
+  },
+  "flyer-sage-interview-workshop.png":
+  {
+    date: "2012-03",
+    text: "Designed flyers for the Student Alliance of GLBT Engineers. This flyer was used to for an Interview Workshop for GLBTQ and Ally Students"
+  },
+  "flyer-sage-resume-workshop.png":
+  {
+    date: "2011-11",
+    text: "Designed flyers for the Student Alliance of GLBT Engineers. This flyer was for a Resume Writing for GLBTQ and Ally Students workshop, which was a collaborative effort between SAGE, the CU GLBTQ Business Leaders Alliance and the University of Colorado Career Services"
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Knitting Man Logo
+  "logo-knitting-man.png":
+  {
+    date: "2012-08",
+    text: "Designed the logo for Knitting Man: A Gay Men's Knitting Group"
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Freedom Swing Postcard
+  "postcard-mhfb-freedom-swing-formal-affair.png":
+  {
+    date: "2014-01",
+    text: "Designed promotional materials for Freedom Swing, the Jazz Band arm of the Mile High Freedom Band. This postcard was used to send to community members to promote the Formal Affair Swing Jazz concert"
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Poster Quotes
+  "poster-quote-anonymous.png":
+  {
+    date: "2013-07",
+    text: "The poster was a side project while designing my business cards. Poster reads, \"Everything will be okay in the end. If it's not okay, it's not the end\", by Anonymous"
+  },
+  "poster-quote-zigler.png":
+  {
+    date: "2013-07",
+    text: "The poster was a side project while designing my business cards. Poster reads, \"The chief cause of failure and unhappiness is trading what you want most for what you want right now\" by Zig Ziglar"
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // GeoStan Grails Application
+  "grails-geostan-01-home.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Homepage",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>Here we see the homepage. Users can search for an address or point using the search bar."
+  },
+  "grails-geostan-02-hampton.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Interpretation",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>This location shows the results between Pitney Bowes' geocoder and Google's geocoder. Which result do you think is more correct based on what the user put in?"
+  },
+  "grails-geostan-03-reverse.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Reverse Geocoding",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>The grails application has support for reverse geocoding. This means you can search for a latitude and longitude point and the geocoders will try an find the closest address."
+  },
+  "grails-geostan-04-infobox.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Infoboxes",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>If a user hovers over any of the points, they can see the result in an info-box."
+  },
+  "grails-geostan-05-intersection.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Intersections",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>There is support for intersections. Here, GeoStan found two possible matches for the user input while Google picked a result for the user. Which result do you think is more correct?"
+  },
+  "grails-geostan-06-multimatch.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Multi-matches",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>Here is a multi-match result. GeoStan found three possible results while Google picked one for the user. Which result do you think is more correct based on what the user put in?"
+  },
+  "grails-geostan-07-drop-pins.png":
+  {
+    date: "2014-01",
+    title: "PB GeoStan<br>&sect; Drop Pins",
+    text: "Created a grails application to showcase Pitney Bowes' geocoder, GeoStan.<br><br>This grails application has support for droping pins on the map. The user then can click any of the markers to run a reverse geocoder run."
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // JamWalkr
+  "lamp-jamwalkr-01.png":
+  {
+    title: "JamWalkr",
+    date: "2012-12",
+    text: "Screen shot of the homepage. Users can click the jars to find out more about the building."
+  },
+  "lamp-jamwalkr-02.png":
+  {
+    title: "JamWalkr",
+    date: "2012-12",
+    text: "Screen shot of the homepage. Users can click the jars to find out more about the building."
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // OpenGL Graphics
+  "opengl-graphics-animation.png":
+  {
+    title: "Computer Graphics",
+    date: "2012-06",
+    text: "In computer Graphics, we learned the OpenGL 2.0. My final project was an animation of a tornado."
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Digital Media 2
+  "processing-generative-art.gif":
+  {
+    date: "2013-10",
+    link: "http://redwood.colorado.edu/vargasp/dm2/proj1/",
+    title: "Generative Art",
+    text: "Developed an interactive, piece of generative art. The project uses characters the user will type and draw them as a square and color."
+  },
+  "processing-new-life-form.gif":
+  {
+    date: "2013-11",
+    link: "http://redwood.colorado.edu/vargasp/dm2/lab6/",
+    title: "Organic Motion",
+    text: "Developed an animation of a new-life form."
+  },
+  "processing-portrait.gif":
+  {
+    date: "2013-09",
+    link: "http://redwood.colorado.edu/vargasp/dm2/lab1/",
+    title: "Conceptual Self Portrait",
+    text: "Developed an interactive, conceptual self-portrait."
+  },
+  "processing-video.gif":
+  {
+    date: "2013-12",
+    link: "http://redwood.colorado.edu/vargasp/dm2/proj3/",
+    title: "Acrylic Video",
+    text: "For the final project I chose to do some video manipulation. I really like the look acrylic makes when stacked on top of each other, creating another image. Video clips from BBC Motion Gallery.<br><br>I interlaced video clips with stock footage to make a mosaic of interesting images. The main reason for this program was to explore video manipulation. I really enjoyed the bit mashing of the different images and found some really interesting techniques along the way. "
+  },
+
+  /////////////////////////////////////////////////////////////////////////////
+  // ATLS Courses
+  "dm1-sequential-images.png":
+  {
+    date: "2013-02",
+    text: "In my Digital Media One course, we learned Photoshop. This project, \"Sequential Images\", we had to compose a pair of images using at least one image found online, one scanned image and one image we took ourselves."
+  },
+  "fodd-unconventional-typeface.jpg":
+  {
+    date: "2014-02",
+    text: "Our task was to pick a word and depict it using an physical, unconventional typeface. I chose \"Moxie\", since you've gotta have moxie in order to win at cards!"
+  }
+
+};
