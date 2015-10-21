41e47d2b595cb50457a8a6b814145f2f117889de
Patrick Vargas <patrick@vargascorpus.com>
2015-10-20 21:01:17 -0600

    Initialized project with Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name northAmericanNinjaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the northAmericanNinjaApp
+ */
+angular.module('northAmericanNinjaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
