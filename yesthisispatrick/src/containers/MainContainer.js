92969eb20e6b4622717a279c6178b34d81d91b05
Patrick Vargas <pevargas90@gmail.com>
2018-02-04 23:33:21 -0500

    Add home and main containers

---
+++ yesthisispatrick/src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../componenets/Main'
+
+class MainContainer extends React.Component {
+  render () {
+    return (
+      <Main />
+    )
+  }
+}
+
+export default MainContainer
