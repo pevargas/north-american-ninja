2c471750297b872b2c0606c922bd84be6837ed4b
Patrick Vargas <pevargas90@gmail.com>
2018-02-04 23:32:53 -0500

    Add AppBar

---
+++ yesthisispatrick/src/components/AppBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+
+class AppBarIcon extends React.Component {
+  render () {
+    return (
+      <AppBar title='Patrick E. Vargas'>
+        <h1>Patrick E. Vargas</h1>
+      </AppBar>
+    )
+  }
+}
+
+export default AppBarIcon
