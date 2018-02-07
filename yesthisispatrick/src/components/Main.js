2acda3ff65fbdaa93be2c731f3783320bb24c074
Patrick Vargas <pevargas90@gmail.com>
2018-02-06 20:26:29 -0500

    Add reboot to main

--- yesthisispatrick/src/components/Main.js
+++ yesthisispatrick/src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AppBar from './AppBar'
+import Reboot from 'material-ui/Reboot'
 
 class Main extends React.Component {
   render () {
     return (
       <div>
+        <Reboot />
         <header>
           <AppBar />
         </header>
