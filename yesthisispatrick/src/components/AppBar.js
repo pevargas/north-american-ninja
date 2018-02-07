9ffe4a8f2ee2900966335856ac92ac0d0c80f6be
Patrick Vargas <pevargas90@gmail.com>
2018-02-06 19:44:10 -0500

    Update navigation bar

--- yesthisispatrick/src/components/AppBar.js
+++ yesthisispatrick/src/components/AppBar.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui-icons/Menu'
 
 class AppBarIcon extends React.Component {
   render () {
     return (
-      <AppBar title='Patrick E. Vargas'>
-        <h1>Patrick E. Vargas</h1>
+      <AppBar title='Patrick E. Vargas' position='static'>
+        <Toolbar>
+          <IconButton color='inherit' aria-label='Menu'>
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant='title' color='inherit'>
+            Patrick E. Vargas
+          </Typography>
+        </Toolbar>
       </AppBar>
     )
   }
