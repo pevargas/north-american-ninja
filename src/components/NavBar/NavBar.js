42ccb0507a38e284e3123d472f42f518c1c4b2e7
Patrick Vargas <pevargas90@gmail.com>
2018-02-07 22:57:52 -0500

    Update page and navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
@@ -8,17 +9,24 @@ import MenuIcon from 'material-ui-icons/Menu'
 class NavBar extends React.Component {
   render () {
     return (
-      <AppBar title='Patrick E. Vargas' position='static'>
-        <Toolbar>
-          <IconButton color='inherit' aria-label='Menu'>
-            <MenuIcon />
-          </IconButton>
+      <nav>
+        <AppBar title='Patrick E. Vargas' position='static'>
+          <Toolbar>
+            <IconButton color='inherit' aria-label='Menu'>
+              <MenuIcon />
+            </IconButton>
 
-          <Typography variant='title' color='inherit'>
-            Patrick E. Vargas
-          </Typography>
-        </Toolbar>
-      </AppBar>
+            <Typography variant='title' color='inherit'>
+              Patrick E. Vargas
+            </Typography>
+
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/resume'>Resume</Link></li>
+            </ul>
+          </Toolbar>
+        </AppBar>
+      </nav>
     )
   }
 }
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from '../Home/Home'
+import Resume from '../Resume/Resume'
 
 class Page extends React.Component {
   render () {
     return (
-      <div>
-        <h1>Page</h1>
-      </div>
+      <Switch>
+        <Route exact path='/' componenet={Home} />
+        <Route path='/resume' componenet={Resume} />
+      </Switch>
     )
   }
 }
